e13b649c9dfb1aa323a7169a38d5c1bf0d178438
zumuya <zumuya@gmail.com>
2022-06-09 21:25:01 +0900

    [Package] Updated targets in library.

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
 		// Products define the executables and libraries a package produces, and make them visible to other packages.
 		.library(
 			name: "InlineLocalization",
-			targets: ["InlineLocalization"]
+			targets: ["InlineLocalization", "InlineLocalizationUI"]
 		),
 	],
 	dependencies: [
